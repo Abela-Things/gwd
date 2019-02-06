6aab974129cfb6899b1933ab1bdb449c95a02708
Julien Sagot <julien.sagot@geneanet.org>
2019-02-06 16:08:39 +0100

    Tests: Enhanced test_date_compare

--- tests/test_data.ml
+++ tests/test_data.ml
@@ -9,9 +9,9 @@ open Test_utils
 
 let field = Jg_runtime.jg_obj_lookup
 
-let assert_equal_tvalue =
+let assert_equal_tvalue ?(printer = Jg_types.show_tvalue) a b =
   let cmp a b = jg_eq_eq a b = Tbool true in
-  assert_equal ~cmp ~printer:Jg_types.show_tvalue
+  assert_equal ~cmp ~printer a b
 
 let test_mk_date _ctx =
   let rec test ({ year ; month ; day ; prec ; _ } as d) =
@@ -27,13 +27,19 @@ let test_mk_date _ctx =
   test (dmy 2 7 1988)
 
 let test_date_compare _ctx =
-  let test exp a b =
-    assert_equal_tvalue (Tint exp) (date_compare_aux (date a) (date b))
+  let test loc exp a b =
+    let printer x = Printf.sprintf "%s -- %s" (Jg_types.show_tvalue x) loc in
+    let assert_equal_tvalue = assert_equal_tvalue ~printer in
+    assert_equal_tvalue (Tint exp) (date_compare_aux (date a) (date b)) ;
+    assert_equal_tvalue (Tint (-exp)) (date_compare_aux (date b) (date a)) ;
+    assert_equal_tvalue (Tint 0) (date_compare_aux (date a) (date a)) ;
+    assert_equal_tvalue (Tint 0) (date_compare_aux (date b) (date b))
   in
-  test 0 (dmy 2 7 1988) (dmy 2 7 1988) ;
-  test (-1) (dmy 2 7 1988) (dmy 3 7 1988) ;
-  test 1 (dmy 2 7 1988) (dmy 1 7 1988) ;
-  test 1 (dmy ~prec:After 0 0 1892) (dmy 15 2 1892)
+  test __LOC__ 0 (dmy 2 7 1988) (dmy 2 7 1988) ;
+  test __LOC__ (-1) (dmy 2 7 1988) (dmy 3 7 1988) ;
+  test __LOC__ 1 (dmy 2 7 1988) (dmy 1 7 1988) ;
+  test __LOC__ 1 (dmy ~prec:After 0 0 1892) (dmy 15 2 1892) ;
+  test __LOC__ 1 (dmy ~prec:After 0 0 1892) (dmy ~prec:After 15 2 1892)
 
 let suite =
   "test_data" >:::
