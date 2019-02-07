7535eb30c363bdaa17bbe78dc04ef2add0affb6d
Julien Sagot <julien.sagot@geneanet.org>
2019-02-07 16:20:29 +0100

    Added handler for [b] and [oa] modes.

--- src/handler.ml
+++ src/handler.ml
@@ -1,5 +1,8 @@
 open Geneweb
+open Gwdb
 open Config
+open Jingoo
+open Jg_types
 
 let restricted_wizard fn self conf base =
   if conf.wizard then fn self conf base
@@ -10,11 +13,53 @@ let with_person self conf base fn =
   | Some p -> fn p
   | None -> self.RequestHandler.very_unknown self conf base
 
+let birth_death_aux conf base fn bool =
+  (* FIXME: do not always load ? *)
+  let () = load_persons_array base in
+  let list =
+    List.map
+      (fun (p, d, c) ->
+         let person = Data.get_n_mk_person conf base (Gwdb.get_key_index p) in
+         let date = Data.mk_date (Dgreg (d, c) ) in
+         Tpat (function
+             | "person" -> person
+             | "date" -> date
+             | _ -> raise Not_found) )
+      (fst @@ BirthDeath.select conf base fn bool)
+  in
+  (* FIXME: do not always load ? *)
+  let () = clear_persons_array base in
+  list
+
 let handler =
   let open RequestHandler in
   { defaultHandler with
 
-    fallback = begin fun mode -> fun self conf base ->
+    _no_mode = defaultHandler._no_mode
+
+  ; b = begin fun _self conf base ->
+      let data = birth_death_aux conf base (fun p -> Adef.od_of_cdate (get_birth p)) false in
+      let models = ("data", Tlist data) :: Data.default_env conf base in
+      Interp.render ~file:"latest_birth.html.jingoo.marshaled" ~models
+    end
+
+  ; oa = begin fun _self conf base ->
+      let limit = Opt.default 0 (Util.p_getint conf.env "lim") in
+      let get_oldest_alive p = match get_death p with
+        | NotDead -> Adef.od_of_cdate (get_birth p)
+        | DontKnowIfDead when limit > 0 ->
+          begin match Adef.od_of_cdate (get_birth p) with
+            | Some (Dgreg (d, _)) as x when conf.today.year - d.year <= limit -> x
+            | _ -> None
+          end
+        | _ -> None
+      in
+      let data = birth_death_aux conf base get_oldest_alive true in
+      let models = ("data", Tlist data) :: Data.default_env conf base in
+      Interp.render ~file:"oldest_alive.html.jingoo.marshaled" ~models
+    end
+
+  ; fallback = begin fun mode -> fun self conf base ->
       match mode with
 
       | "SANDBOX" ->
